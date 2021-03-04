c2a90c6b88eb4241327a02f40548d80052990956
Wessel Dankers <wsl@fruit.je>
2021-03-04 22:23:23 +0100

    drm: fix brightness conversion

--- video/out/drm_atomic.c
+++ video/out/drm_atomic.c
@@ -507,13 +507,13 @@ void drm_send_hdrmeta(struct drm_atomic_context *ctx, struct mp_colorspace *colo
      
 
     if(p->data.hdmi_metadata_type1.eotf) {
-
+        // Convert to nits (cd/m2):
         float min_luminance = 0;	// ??? Is it correct?
-        float max_luminance = color->sig_peak / 100.0; // [0..1]; mpv makes color management, so, all levels are the same?
-        float max_cll = color->sig_peak / 100.0;
-        float max_fall = color->sig_peak / 100.0;
+        float max_luminance = color->sig_peak * 100.0;
+        float max_cll = color->sig_peak * 100.0;
+        float max_fall = color->sig_peak * 100.0;
         
-        // Convert to  "16-bit values in units of 0.00002, where 0x0000 represents zero and 0xC350 represents 1.0000"
+        // Convert to 16-bit values in units of 0.00002, where 0x0000 represents zero and 0xC350 (50000) represents 1.0000
         p->data.hdmi_metadata_type1.display_primaries[0].x = round(colors.red.x * 50000.0);
         p->data.hdmi_metadata_type1.display_primaries[0].y = round(colors.red.y * 50000.0);
         p->data.hdmi_metadata_type1.display_primaries[1].x = round(colors.green.x * 50000.0);
@@ -524,18 +524,21 @@ void drm_send_hdrmeta(struct drm_atomic_context *ctx, struct mp_colorspace *colo
         p->data.hdmi_metadata_type1.white_point.x = round(colors.white.x * 50000.0);
         p->data.hdmi_metadata_type1.white_point.y = round(colors.white.y * 50000.0);
 
-        //... "16-bit value in units of 1 cd/m2, where 0x0001 represents 1 cd/m2 and 0xFFFF represents 65535 cd/m2"
-        if(max_luminance >= 0)
-            p->data.hdmi_metadata_type1.max_display_mastering_luminance = round(max_luminance * 65535);
-        //... "16-bit value in units of 0.0001 cd/m2, where 0x0001 represents 0.0001 cd/m2 and 0xFFFF represents 6.5535 cd/m2"
+        // Min Luminance: 16-bit value in units of 0.0001 cd/m2, where 0x0001 represents 0.0001 cd/m2 and 0xFFFF represents 6.5535 cd/m2.
         if(min_luminance >= 0)
-            p->data.hdmi_metadata_type1.min_display_mastering_luminance = round(min_luminance * 65535);
-        //... "16-bit value in units of 1 cd/m2, where 0x0001 represents 1 cd/m2 and 0xFFFF represents 65535 cd/m2."
+            p->data.hdmi_metadata_type1.min_display_mastering_luminance = round(min_luminance * 10000.0);
+
+        // Max Luminance: 16-bit value in units of 1 cd/m2, where 0x0001 represents 1 cd/m2 and 0xFFFF represents 65535 cd/m2.
+        if(max_luminance >= 0)
+            p->data.hdmi_metadata_type1.max_display_mastering_luminance = round(max_luminance);
+
+        // Max Content Light Level: 16-bit value in units of 1 cd/m2, where 0x0001 represents 1 cd/m2 and 0xFFFF represents 65535 cd/m2.
         if (max_cll >= 0)
-            p->data.hdmi_metadata_type1.max_cll = round(max_cll * 65535);
-        //... "16-bit value in units of 1 cd/m2, where 0x0001 represents 1 cd/m2 and 0xFFFF represents 65535 cd/m2."
+            p->data.hdmi_metadata_type1.max_cll = round(max_cll);
+
+        // Max Frame Average Light Level: 16-bit value in units of 1 cd/m2, where 0x0001 represents 1 cd/m2 and 0xFFFF represents 65535 cd/m2.
         if (max_fall >= 0)
-            p->data.hdmi_metadata_type1.max_fall = round(max_fall * 65535);
+            p->data.hdmi_metadata_type1.max_fall = round(max_fall);
        
        drmModeCreatePropertyBlob(ctx->connector->fd, &p->data, sizeof(p->data), &p->blob_id);
        drmModeAtomicReqPtr request = drmModeAtomicAlloc();
